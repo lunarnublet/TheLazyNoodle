002f0a42510784198c096dd8d9c978b7e10c3ea2
Tryston Maris <tmaris@student.neumont.edu>
2016-11-08 17:57:30 -0700

    Can Search.

--- Library Project/Library Project/Controllers/HomeController.cs
+++ Library Project/Library Project/Controllers/HomeController.cs
@@ -18,5 +18,37 @@ namespace Library_Project.Controllers
             }
             return View(books);
         }
+
+        [HttpPost]
+        public ActionResult Index(Book book)
+        {
+            List<Book> books = new List<Book>();
+            using (TheLazyNoodleEntities1 con = new TheLazyNoodleEntities1())
+            {
+                books = con.Books.ToList();
+                if (book.Title != null && book.Title != "")
+                {
+                    List<Book> temp = con.Books.Where(b => b.Title.Contains(book.Title)).ToList();
+                    books = temp.Intersect(books).ToList();
+                }
+                if (book.PublishYear != null)
+                {
+                    List<Book> temp = con.Books.Where(b => b.PublishYear == book.PublishYear).ToList();
+                    books = temp.Intersect(books).ToList();
+                }
+            }
+            return View("Index", books);
+        }
+
+        public ActionResult Search()
+        {
+            return View(new Book());
+        }
+
+        [HttpPost]
+        public ActionResult Search(Book book)
+        {
+            return Index(book);
+        }
     }
 }
