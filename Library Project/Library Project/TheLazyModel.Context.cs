3af8063065eb85cfb9960805bdb758468fe7c785
Tryston Maris <tmaris@student.neumont.edu>
2016-11-15 17:20:36 -0700

    Added UserProfiles to the database and a joining table with books and UserProfiles

--- Library Project/Library Project/TheLazyModel.Context.cs
+++ Library Project/Library Project/TheLazyModel.Context.cs
@@ -27,5 +27,6 @@ namespace Library_Project
     
         public virtual DbSet<Book> Books { get; set; }
         public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<UserProfile> UserProfiles { get; set; }
     }
 }
